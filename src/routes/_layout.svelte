34b83eabbc3e85d55068db41020d9377570f4026
Chris Milkaitis <chris.milkaitis@gmail.com>
2020-10-26 21:33:06 -0400

    Add new api info on card, display image, import tailwind, yikes

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -10,3 +10,16 @@
 		</div>
 	</Footer>
 </MaterialApp>
+
+<style global lang="postcss">
+
+	/* only apply purgecss on utilities, per Tailwind docs */
+	/* purgecss start ignore */
+	@tailwind base;
+	@tailwind components;
+	/* purgecss end ignore */
+  
+	@tailwind utilities;
+  
+  </style>
+  
