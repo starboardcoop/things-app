c531a04618b3c641b6c8e279556f61b45014ed39
mdantuono <mikeishere3@gmail.com>
2020-11-02 22:45:34 -0500

    little movement never hurt

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -2,7 +2,7 @@
     export let primary = false;
 </script>
 
-<button class:primary class="cursor-pointer bg-white font-bold px-4 py-2 rounded-sm shadow-md hover:shadow-lg">
+<button class:primary class="cursor-pointer bg-white font-bold px-4 py-2 rounded-sm shadow-md hover:shadow-lg transform hover:scale-105 duration-200">
     <slot />
 </button>
 
