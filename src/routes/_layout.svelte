2d69cdd7f2d4b3a468899c0787530ae4c1bb546d
Dillon Fagan <dillonfagan@pm.me>
2020-11-16 18:37:33 -0500

    Update root styles

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -13,19 +13,15 @@
 </Layout>
 
 <style global lang="postcss">
-
 	@tailwind base;
 	@tailwind components;
-  
 	@tailwind utilities;
 
-	html {
+	:root {
 		font-family: 'Noto Serif', serif;
 	}
 	
 	h1, h2, h3, h4, h5, h6 {
 		font-family: 'Roboto Mono', monospace;
 	}
-  
-  </style>
-  
+</style>
