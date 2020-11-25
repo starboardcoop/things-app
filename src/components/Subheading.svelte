3b33f0fa9a046ca1b54403685fb15716a65af48d
Dillon Fagan <dillonfagan@pm.me>
2020-11-25 14:57:21 -0500

    Add center prop to Subheading

--- src/components/Subheading.svelte
+++ src/components/Subheading.svelte
@@ -1,3 +1,13 @@
-<h3 class="text-xl font-bold text-gray-500">
+<script>
+    export let center = false;
+</script>
+
+<h3 class:center class="text-xl font-bold text-gray-500">
     <slot />
 </h3>
+
+<style>
+    h3.center {
+        @apply text-center;
+    }
+</style>
