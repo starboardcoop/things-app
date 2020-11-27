93e08167ecb6537f096c63cebfb83c5d94e7b4f0
Dillon Fagan <dillonfagan@pm.me>
2020-11-27 17:59:43 -0500

    Add caps and bold props to Subheading

--- src/components/Subheading.svelte
+++ src/components/Subheading.svelte
@@ -1,12 +1,23 @@
 <script>
+    export let caps = false;
+    export let bold = false;
     export let center = false;
+    export let color = "inherit";
 </script>
 
-<h3 class:center class="text-xl font-bold text-indigo-600">
+<h3 class:caps class:bold class:center class={`text-xl font-bold text-${color}`}>
     <slot />
 </h3>
 
 <style>
+    .caps {
+        @apply uppercase;
+    }
+
+    .bold {
+        @apply font-bold;
+    }
+
     h3.center {
         @apply text-center;
     }
