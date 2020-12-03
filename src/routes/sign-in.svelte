0f94efa8ff427d5206c3d177cb9346ce7636be06
mdantuono <mikeishere3@gmail.com>
2020-12-02 20:35:33 -0500

    Create sign in page, add number input

---
+++ src/routes/sign-in.svelte
@@ -0,0 +1,19 @@
+<style>
+    .solid {
+      box-shadow: 2px 2px 0 #000000;
+    }
+  
+    .solid:focus, .solid:hover {
+      box-shadow: 4px 4px 0 #000000;
+    }
+  </style>
+  
+  <main class="bg-indigo-300 w-screen h-screen font-mono">
+    <div class="w-full h-full flex flex-col justify-center items-center p-8">
+      <div class="flex flex-row gap-4 flex-wrap">
+        <input type="tel" placeholder="(401) 555-5555" pattern="([0-9]{3}) [0-9]{3}-[0-9]{4}" class="px-4 py-2 border-2 border-black solid rounded-md outline-none transform hover:scale-105 duration-200" />
+        <button class="bg-yellow-300 px-4 py-2 border-2 border-black solid rounded-md uppercase font-bold transform hover:scale-105 duration-200">Submit</button>
+      </div>
+    </div>
+  </main>
+  
