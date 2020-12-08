b0ed8b30a3aeec7710378b6ffcebc2b36ce74290
mdantuono <mikeishere3@gmail.com>
2020-12-07 21:12:42 -0500

    Display cards on the dashboard

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -1,8 +1,12 @@
 <script>
     import { onMount } from "svelte";
+    import Card from "../components/Card.svelte";
+    import Container from "../components/Container.svelte";
     import Session from "../session";
 
     let name;
+
+    let cards = new Array(20).fill("card");
 
     onMount(() => {
         const session = Session.json();
@@ -12,7 +16,14 @@
 </script>
 
 <main class="bg-indigo-300 w-screen h-screen font-mono">
-    <div class="w-full h-full flex flex-col justify-center items-center p-8">
+    <div class="w-full flex flex-col justify-center items-center p-8 overflow-hidden">
         <h1>Hi, {name}!</h1>
+    </div>
+    <div class="flex flex-row gap-3 overflow-scroll px-8 py-7">
+        {#each cards as card}
+        <Card>
+            <Container>{card}</Container>
+        </Card>
+        {/each}
     </div>
 </main>
