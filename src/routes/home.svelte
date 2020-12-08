9c538f2d43dfcf47612f893ce577672f9b107561
mdantuono <mikeishere3@gmail.com>
2020-12-07 22:02:27 -0500

    Things displaying in cards

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -5,6 +5,7 @@
     import Session from "../session";
 
     let name;
+    let data = thingify();
 
     let cards = new Array(20).fill("card");
 
@@ -16,20 +17,33 @@
 
     async function thingify() {
         const result = await fetch(`/.netlify/functions/things`);
-        const data = await result.json();
+        data = await result.json();
         console.log(data);
+        return data;
     }
 </script>
 
 <main class="bg-indigo-300 w-screen h-screen font-mono">
-    <div class="w-full flex flex-col justify-center items-center p-8 overflow-hidden">
-        <h1>Hi, {name}!</h1>
-    </div>
-    <div class="flex flex-row gap-3 overflow-scroll px-8 py-7">
-        {#each cards as card}
-        <Card>
-            <Container><a href="/home/#" on:click={thingify}>{card}</a></Container>
-        </Card>
-        {/each}
+    <div>
+        <div class="w-full flex flex-col justify-center items-center p-8 overflow-hidden">
+            <h1>Hi, {name}!</h1>
+        </div>
+        <h2>Recommended Things:</h2>
+        <div class="flex flex-row gap-3 overflow-scroll px-8 py-7">
+            {#await data}
+                loading...
+            {:then things}
+                {#each data.things as thing}
+                <Card>
+                    <Container>
+                        <img src={thing.img} alt="a thing"/>
+                        {thing.name}
+                    </Container>
+                </Card>
+                {/each}
+            {:catch error}
+                whoops!: {error}
+            {/await}
+        </div>
     </div>
 </main>
