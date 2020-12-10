0180b6f5b788a278a7df6c462c1a59ff63e748ed
mdantuono <mikeishere3@gmail.com>
2020-12-09 20:21:26 -0500

    Pull Thing descriptions out of the cards

--- src/routes/home.svelte
+++ src/routes/home.svelte
@@ -33,35 +33,37 @@
         <Container>
             <h2>Recommended Things:</h2>
         </Container>
-            <div class="flex flex-row gap-3 overflow-scroll px-8 py-7">
-                {#await data}
-                    loading...
-                {:then}
-                    {#each data.things as thing}
-                    <Card>
-                        <Container>
-                            <Image height="25" src={thing.img} alt={thing.name} />
-                            <Subheading>{thing.name}</Subheading>
-                            <Text small>{thing.category}</Text>
-                        </Container>
-                        <div class="flex flex-col space-y-5" slot="modal">
-                            <Subheading bold>pvd<span class="text-indigo-600">:</span>thing</Subheading>
-                            <Heading bold>{thing.name}</Heading>
-                            <span>
-                                <Text small>Available:</Text>
-                                <Text>Coming Soon!</Text>
-                            </span>
-                            <span>
-                                <Text small>Typical out-of-pocket cost:</Text>
-                                <Text>{thing.price}</Text>
-                            </span>
+        <div class="flex flex-row gap-3 overflow-scroll px-8 py-7">
+            {#await data}
+                loading...
+            {:then}
+                {#each data.things as thing}
+                    <div>
+                        <Card height="24" width="24">
+                            <Image height="full" src={thing.img} alt={thing.name} />
+                            <div class="flex flex-col space-y-5" slot="modal">
+                                <Subheading bold>pvd<span class="text-indigo-600">:</span>thing</Subheading>
+                                <Heading bold>{thing.name}</Heading>
+                                <span>
+                                    <Text small>Available:</Text>
+                                    <Text>Coming Soon!</Text>
+                                </span>
+                                <span>
+                                    <Text small>Typical out-of-pocket cost:</Text>
+                                    <Text>{thing.price}</Text>
+                                </span>
+                                <Text small>{thing.category}</Text>
+                            </div>
+                        </Card>
+                        <div class="pl-1 pt-2">
+                            <Text bold>{thing.name}</Text>
                             <Text small>{thing.category}</Text>
                         </div>
-                    </Card>
-                    {/each}
-                {:catch error}
-                    whoops!: {error}
-                {/await}
-            </div>
+                    </div>
+                {/each}
+            {:catch error}
+                whoops!: {error}
+            {/await}
+        </div>
     </div>
 </main>
