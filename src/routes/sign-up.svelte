94c36982f6b79dd58b92fbb819c4e39d19f9550f
mdantuono <mikeishere3@gmail.com>
2020-12-16 21:08:51 -0500

    Add layout to sign-up page

--- src/routes/sign-up.svelte
+++ src/routes/sign-up.svelte
@@ -1,1 +1,11 @@
-<p>Sign up!</p>
+<main class="bg-indigo-300 w-screen h-screen font-mono">
+    <div class="w-full h-full flex flex-col justify-center items-center p-8">
+        <div class="flex flex-row gap-4 flex-wrap">
+            <input
+                type="text"
+                placeholder="Name"
+                class="px-4 py-2 brutal hovers outline-none" />
+            <button class="bg-yellow-300 px-4 py-2 brutal hovers uppercase font-bold">Sign up</button>
+        </div>
+    </div>
+</main>
