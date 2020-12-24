4d9381e688f0aff18dcc0bf4dc1bc07748e79dce
Chris Milkaitis <chris.milkaitis@gmail.com>
2020-12-23 20:05:06 -0500

    Add condition for bad access code

--- functions/auth-code.js
+++ functions/auth-code.js
@@ -1,4 +1,5 @@
 import fetch from "node-fetch";
+import { element_is } from "svelte/internal";
 
 export async function handler(event, context, callback) {
     console.log("Authenticating code...");
@@ -22,8 +23,16 @@ export async function handler(event, context, callback) {
 
     const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 
+    if (code != "12345")
+    {
+        return {
+            statusCode: 401,
+            body: JSON.stringify({error: `Your access code is invalid.`})
+        }
+    }
+     
     return {
         statusCode: 200,
         body: JSON.stringify({ member: member, token: token })
     }
-}
+} 
