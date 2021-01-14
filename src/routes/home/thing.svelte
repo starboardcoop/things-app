04ffd10984f2b4087feb346f37075cf8f1e31cd3
Chris Milkaitis <chris.milkaitis@gmail.com>
2021-01-13 22:13:23 -0500

    Style thing page

--- src/routes/home/thing.svelte
+++ src/routes/home/thing.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount } from "svelte";
     import Heading from "../../components/Heading.svelte";
+    import Image from "../../components/Image.svelte";
     import Subheading from "../../components/Subheading.svelte";
     import Text from "../../components/Text.svelte";
 
@@ -15,16 +16,22 @@
     })
 </script>
 
-<div class="flex flex-col space-y-5">
-    <Subheading bold>pvd<span class="text-indigo-600">:</span>thing</Subheading>
-    <Heading bold>{thing.name}</Heading>
-    <span>
-        <Text small>Available:</Text>
-        <Text>Coming Soon!</Text>
-    </span>
-    <span>
-        <Text small>Typical out-of-pocket cost:</Text>
-        <Text>{thing.price}</Text>
-    </span>
-    <Text small>{thing.category}</Text>
-</div>
+<main class="bg-indigo-300 w-screen min-h-screen font-mono">
+    <div>
+        <div class="w-full flex flex-col justify-center items-center p-8 bg-bg relative">
+            <Heading color="white">{thing.name}</Heading>
+        </div>
+        <div class="flex flex-col space-y-5 mt-10 p-8">
+            <Image src={thing.img} alt={thing.name} />
+            <span>
+                <Text small>Available:</Text>
+                <Text>Coming Soon!</Text>
+            </span>
+            <span>
+                <Text small>Typical out-of-pocket cost:</Text>
+                <Text>{thing.price}</Text>
+            </span>
+            <Text small>{thing.category}</Text>
+        </div>
+    </div>
+</main>
