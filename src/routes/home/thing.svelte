d24ff7d3f4f6f8cf72e02174ed088b26262664dc
mdantuono <mikeishere3@gmail.com>
2021-01-20 21:21:39 -0500

    Center page and add padding

--- src/routes/home/thing.svelte
+++ src/routes/home/thing.svelte
@@ -7,6 +7,7 @@
     import Subheading from "../../components/Subheading.svelte";
     import Text from "../../components/Text.svelte";
     import Column from "../../components/Column.svelte";
+    import Section from "../../components/Section.svelte";
 
     let thing = {}
 
@@ -19,24 +20,26 @@
     })
 </script>
 
+<div class="w-full flex flex-col justify-center items-center p-8 bg-bg relative text-white">
+    <Heading>pvd<span class="text-primary">:</span>thing</Heading>
+</div>
 <Column spacing="0">
-    <div class="w-full flex flex-col justify-center items-center p-8 bg-bg relative text-white">
-        <Heading>pvd<span class="text-primary">:</span>thing</Heading>
-    </div>
-    <div class="flex flex-col space-y-5 mt-10 p-8">
-        <div on:click={ () => goto('/home') }>back</div>
-        <div>
-            <Subheading>{thing.name}</Subheading>
-            <Text small>{thing.category}</Text>
+    <Section>
+        <div class="flex flex-col space-y-5 mt-10 p-8">
+            <div on:click={ () => goto('/home') }>back</div>
+            <div>
+                <Subheading>{thing.name}</Subheading>
+                <Text small>{thing.category}</Text>
+            </div>
+            <Image src={thing.img} alt={thing.name} />
+            <span>
+                <Text small>Available:</Text>
+                <Text>Coming Soon!</Text>
+            </span>
+            <span>
+                <Text small>Typical out-of-pocket cost:</Text>
+                <Text>{thing.price}</Text>
+            </span>
         </div>
-        <Image src={thing.img} alt={thing.name} />
-        <span>
-            <Text small>Available:</Text>
-            <Text>Coming Soon!</Text>
-        </span>
-        <span>
-            <Text small>Typical out-of-pocket cost:</Text>
-            <Text>{thing.price}</Text>
-        </span>
-    </div>
+    </Section>
 </Column>
