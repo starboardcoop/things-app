31f7d09c5467ec4aa0644b6fb550a7a42c0130c7
Dillon Fagan <dillonfagan@pm.me>
2021-01-25 16:41:57 -0500

    Disallow empty name field

--- src/routes/sign-up.svelte
+++ src/routes/sign-up.svelte
@@ -2,9 +2,19 @@
     import Session from '../session';
     import { goto } from '@sapper/app';
 
+    let invalid = false;
     let nameText = "";
 
+    function hasValidName() {
+        return nameText.length > 0;
+    }
+
     async function submit() {
+        if (!hasValidName()) {
+            invalid = true;
+            return new Promise(() => {});
+        }
+
         const session = Session.json();
 
         const result = await fetch("/.netlify/functions/sign-up", {
@@ -30,10 +40,18 @@
         <div class="flex flex-row gap-4 flex-wrap">
             <input
                 bind:value={nameText}
+                on:change={() => invalid = false}
                 type="text"
                 placeholder="Name"
+                class:invalid
                 class="px-4 py-2 brutal hovers outline-none" />
             <button on:click={submit} class="bg-yellow-300 px-4 py-2 brutal hovers uppercase font-bold">Sign up</button>
         </div>
     </div>
 </main>
+
+<style>
+    .invalid {
+        @apply bg-red-200;
+    }
+</style>
