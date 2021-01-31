5468b70975d894b5c10d2453aa7afd31fbc031d7
Dillon Fagan <dillonfagan@pm.me>
2021-01-31 16:14:59 -0500

    Introduce TextInput

---
+++ src/components/TextInput.svelte
@@ -0,0 +1,18 @@
+<script>
+    export let value = "";
+    export let invalid = false;
+</script>
+
+<input
+    {...$$props}
+    bind:value
+    on:input
+    on:change
+    class:invalid
+    class="px-4 py-2 brutal hovers outline-none" />
+
+<style>
+    .invalid {
+        @apply bg-red-200;
+    }
+</style>
