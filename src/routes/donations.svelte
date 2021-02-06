1ee8a652f3ed38b9828b939eccc5d158bd7143b1
Dillon Fagan <dillonfagan@pm.me>
2021-02-06 10:33:23 -0500

    Add link to donation form

--- src/routes/donations.svelte
+++ src/routes/donations.svelte
@@ -5,6 +5,8 @@
     import Heading from "../components/Heading.svelte";
     import Text from "../components/Text.svelte";
     import Button from "../components/Button.svelte";
+
+    const formURL = "https://docs.google.com/forms/d/e/1FAIpQLSfpUUKhhCr32Z_fE8ogvmDv4bD0icWrgTt7QOZVfaNVZemOcg/viewform?usp=sf_link";
 </script>
 
 <Column spacing="0">
@@ -17,7 +19,11 @@
                 <Heading>Have a Thing to donate?</Heading>
                 <Text>As a nonprofit organization in the early startup phase, Thing donations are very much appreciated. Click the button below to fill out our donation form.</Text>
             </div>
-            <div><Button primary>Donate</Button></div>
+            <div>
+                <Button primary>
+                    <a href={formURL} target="_blank" rel="nofollow">Donate</a>
+                </Button>
+            </div>
         </Column>
     </Section>
 </Column>
