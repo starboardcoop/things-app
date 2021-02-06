5e133930924eb08fe144759f88b686dff71121b8
Dillon Fagan <dillonfagan@pm.me>
2021-02-06 10:21:59 -0500

    Create donations page layout

---
+++ src/routes/donations.svelte
@@ -0,0 +1,23 @@
+<script>
+    import Column from "../components/Column.svelte";
+    import Section from "../components/Section.svelte";
+    import Title from "../components/Title.svelte";
+    import Heading from "../components/Heading.svelte";
+    import Text from "../components/Text.svelte";
+    import Button from "../components/Button.svelte";
+</script>
+
+<Column spacing="0">
+    <Section bg="bg">
+        <Title>Donating Things</Title>
+    </Section>
+    <Section>
+        <Column>
+            <div>
+                <Heading>Have a Thing to donate?</Heading>
+                <Text>As a nonprofit organization in the early startup phase, Thing donations are very much appreciated. Click the button below to fill out our donation form.</Text>
+            </div>
+            <div><Button primary>Donate</Button></div>
+        </Column>
+    </Section>
+</Column>
