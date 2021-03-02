30873698181057bdc086471e92322802ad02804c
Dillon Fagan <dillonfagan@pm.me>
2021-03-01 22:42:31 -0500

    Fix mobile spacing

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -13,7 +13,7 @@
     let visible = false;
 </script>
 
-<div class="flex flex-col space-y-4 fixed bottom-4 md:top-4 right-4 items-end">
+<div class="z-50 flex flex-col-reverse md:flex-col md:space-y-4 fixed bottom-4 md:top-4 right-4 items-end">
     <div on:click={() => visible = !visible} class="h-16 w-16 rounded-full cursor-pointer bg-indigo-300 shadow-lg hover:shadow text-center p-4">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" viewBox="0 0 24 24" stroke-width="1.7" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
@@ -22,7 +22,7 @@
             <line x1="4" y1="18" x2="20" y2="18" />
         </svg>
     </div>
-    <nav class:visible class="bg-white shadow w-max overflow-hidden p-4 rounded-lg hidden flex-col space-y-2">
+    <nav class:visible class="bg-white shadow w-max overflow-hidden p-4 rounded-lg hidden flex-col space-y-2 mb-4">
         {#each pages as page}
             <a href={page.url} class="hover:underline">{page.label}</a>
         {/each}
