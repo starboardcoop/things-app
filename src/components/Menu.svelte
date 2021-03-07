15e4e45d2702c4f1943a9d08cd3625fc02f27fde
Dillon Fagan <dillonfagan@pm.me>
2021-03-07 10:08:18 -0500

    Lighten menu color

--- src/components/Menu.svelte
+++ src/components/Menu.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <div class="z-40 flex flex-col-reverse md:flex-col md:space-y-4 fixed bottom-4 md:top-4 right-4 items-end">
-    <div on:click={() => visible = !visible} class="h-16 w-16 rounded-full cursor-pointer bg-indigo-300 shadow-lg hover:shadow text-center p-4">
+    <div on:click={() => visible = !visible} class="h-16 w-16 rounded-full cursor-pointer bg-indigo-200 shadow-lg hover:shadow text-center p-4">
         <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-menu-2" viewBox="0 0 24 24" stroke-width="1.7" stroke="#000000" fill="none" stroke-linecap="round" stroke-linejoin="round">
             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
             <line x1="4" y1="6" x2="20" y2="6" />
