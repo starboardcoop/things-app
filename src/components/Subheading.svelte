b369036f73b2b9bc45723eec81d8a8770ee5b14c
Dillon Fagan <dillonfagan@pm.me>
2021-03-10 22:41:43 -0500

    Remove unused prop

--- src/components/Subheading.svelte
+++ src/components/Subheading.svelte
@@ -1,21 +1,16 @@
 <script>
     export let caps = false;
-    export let bold = false;
     export let center = false;
     export let color = "inherit";
 </script>
 
-<h3 class:caps class:bold class:center class={`text-xl font-bold text-${color}`}>
+<h3 class:caps class:center class={`text-xl font-bold text-${color}`}>
     <slot />
 </h3>
 
 <style>
     .caps {
         @apply uppercase;
-    }
-
-    .bold {
-        @apply font-bold;
     }
 
     h3.center {
