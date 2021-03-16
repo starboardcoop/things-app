858dec87b0f8c8a2d5a80e823ba2cfb490bdf407
Chris Milkaitis <chris.milkaitis@gmail.com>
2021-03-15 21:37:15 -0400

    Re-style the Title component

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,19 +1,16 @@
 <script>
     import Row from "./Row.svelte";
-    import Section from "./Section.svelte";
     import Title from "./Title.svelte";
     
     export let title;
 </script>
 
-<header class="w-full h-24 p-4 flex flex-col justify-center bg-long-city bg-cover">
+<header class="w-full p-4 flex flex-col space-y-8 justify-center bg-long-city bg-cover shadow">
     <Row center>
-        <img src="/PVD_Things_Logo_White.png" alt="PVD Things" class="h-12"/>
+        <img src="/PVD_Things_Logo_White.png" alt="PVD Things" class="h-14 md:h-20 lg:h-24"/>
         <h1 hidden>PVD Things</h1>
     </Row>
+    {#if title}
+        <Row center><Title bold caps backdrop size="3">{title}</Title></Row>
+    {/if}
 </header>
-{#if title}
-    <Section bg="bg">
-        <Title color="primary">{title}</Title>
-    </Section>
-{/if}
--- src/components/Title.svelte
+++ src/components/Title.svelte
@@ -1,7 +1,24 @@
 <script>
     export let color = "white";
+    export let bold = false;
+    export let caps = false;
+    export let backdrop = false;
 </script>
 
-<h1 class="text-5xl md:text-6x text-{color} text-center leading-normal">
+<h1 class:bold class:caps class:backdrop class="text-3xl md:text-4xl text-{color} text-center max-w-max px-2">
     <slot />
 </h1>
+
+<style>
+    .bold {
+        @apply font-bold;
+    }
+
+    .caps {
+        @apply uppercase;
+    }
+
+    .backdrop {
+        @apply bg-indigo-700;
+    }
+</style>
