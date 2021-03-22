16e39c60a03f26e5d231d0d93934b8c0bb46609f
Dillon Fagan <dillonfagan@pm.me>
2021-03-22 19:45:08 -0400

    Change shadow to light gray line

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -5,7 +5,7 @@
     export let title;
 </script>
 
-<header class="w-full p-4 flex flex-col space-y-8 justify-center bg-long-city bg-cover shadow">
+<header class="w-full p-4 flex flex-col space-y-8 justify-center bg-long-city bg-cover border-b-2 border-gray-200">
     <Row center>
         <img src="/PVD_Things_Logo_White.png" alt="PVD Things" class="h-14 md:h-20 lg:h-24"/>
         <h1 hidden>PVD Things</h1>
