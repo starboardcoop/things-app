95b8c6f396bba5d9088789f36ba718acbbdd8bef
Dillon Fagan <dillonfagan@pm.me>
2021-03-22 19:47:56 -0400

    Link logo to homepage

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -7,7 +7,9 @@
 
 <header class="w-full p-4 flex flex-col space-y-8 justify-center bg-long-city bg-cover border-b-2 border-gray-200">
     <Row center>
-        <img src="/PVD_Things_Logo_White.png" alt="PVD Things" class="h-14 md:h-20 lg:h-24"/>
+        <a href="/">
+            <img src="/PVD_Things_Logo_White.png" alt="PVD Things" class="h-14 md:h-20 lg:h-24"/>
+        </a>
         <h1 hidden>PVD Things</h1>
     </Row>
     {#if title}
