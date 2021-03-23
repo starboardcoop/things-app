1e00e1491df85665a2355da69de29bba565271b1
Chris Milkaitis <chris.milkaitis@gmail.com>
2021-03-22 22:02:45 -0400

    Remove extra modal styles

--- src/components/Card.svelte
+++ src/components/Card.svelte
@@ -2,24 +2,10 @@
     export let height = "auto";
     export let width = "auto";
     export let hovers = true;
-
-    let showModal = false;
-    const toggleModal = () => showModal = !showModal;
 </script>
 
-<div on:click={toggleModal} class={`h-${height} w-${width}`}>
+<div class={`h-${height} w-${width}`}>
     <div class:hovers class="h-full bg-white brutal overflow-hidden">
         <slot />
     </div>
 </div>
-
-<style>
-    div.visible {
-        @apply flex;
-    }
-
-    div.thicc {
-        box-shadow: 4px 4px 0 #000000;
-    }
-</style>
-
