155b0e33507e0620a70ad9e32b30be59b6e1cabd
mdantuono <mikeishere3@gmail.com>
2021-05-31 21:01:46 -0400

    Remove unused components and packages

--- src/components/Container.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    export let bg = "transparent";
-</script>
-
-<div class={`h-full w-full p-4 bg-${bg} justify-center`}>
-    <slot />
-</div>
--- src/components/Grid.svelte
+++
@@ -1,3 +0,0 @@
-<div class={`grid grid-flow-row grid-cols-2 md:grid-cols-3 auto-rows-auto lg:grid-rows-1 gap-4`}>
-    <slot />
-</div>
