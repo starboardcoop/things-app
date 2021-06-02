fa921d7bba568b3eeb07b75afcaff10a5302b7f7
Dillon Fagan <dillonfagan@pm.me>
2021-06-02 16:11:58 -0400

    Increase Card caption width to match Card size on large screen

--- src/components/Scroller.svelte
+++ src/components/Scroller.svelte
@@ -12,7 +12,7 @@
             <Card style="h-24 w-24 lg:h-48 lg:w-48">
                 <Image height="full" src={thing.image} alt={thing.name} />
             </Card>
-            <div class="pl-1 pt-2 w-24">
+            <div class="pl-1 pt-2 w-24 lg:w-48">
                 <Text bold smallauto>{thing.name}</Text>
                 {#if thing.stock > 0}
                     <Text small>{thing.stock} in stock</Text>
