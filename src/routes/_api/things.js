0464043d77948ad52417d542195edc4170eabd41
Dillon Fagan <dillonfagan@pm.me>
2022-11-27 11:17:05 -0500

    Always fetch data from API

--- src/routes/_api/things.js
+++ src/routes/_api/things.js
@@ -1,25 +1,6 @@
-async function getAll() {
-    let data;
-    let now = new Date();
-
-    let previousRefresh = new Date(localStorage.getItem("previousRefresh"));
-    if (Math.abs(now - previousRefresh) > 120000) {
-        data = _getAll();
-        localStorage.setItem("previousRefresh", now.toUTCString());
-    } else {
-        data = JSON.parse(localStorage.getItem("data"));
-
-        console.log('Previous data refreshed.');
-    }
-    
-    return data;
-}
-
-async function _getAll() {
+const getAll = async () => {
     const result = await fetch(`/.netlify/functions/things`);
     let data = await result.json();
-    localStorage.setItem("data", JSON.stringify(data));
-
     data.things = data.things.filter(thing => thing.categories);
 
     console.log('Refreshed data from API.');
