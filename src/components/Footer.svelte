f0e978b217639e26ad4ac71564378ee63a6e23eb
Dillon Fagan <dillonfagan@pm.me>
2022-11-29 10:48:45 -0500

    Remove LinkedIn from Footer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -31,17 +31,6 @@
                     <path d="M7 10v4h3v7h4v-7h3l1 -4h-4v-2a1 1 0 0 1 1 -1h3v-4h-3a5 5 0 0 0 -5 5v2h-3" />
                 </svg>
             </a>
-            <a href="https://linkedin.com/company/starboardcoop/" target="_blank">
-                <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-linkedin" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fdd835" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <title id="linkedInTitleID">LinkedIn</title>
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <rect x="4" y="4" width="16" height="16" rx="2" />
-                    <line x1="8" y1="11" x2="8" y2="16" />
-                    <line x1="8" y1="8" x2="8" y2="8.01" />
-                    <line x1="12" y1="16" x2="12" y2="11" />
-                    <path d="M16 16v-3a2 2 0 0 0 -4 0" />
-                </svg>
-            </a>
             <a href="https://discord.gg/DbYtbFYkJD" target="_blank">
                 <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-brand-discord" width="36" height="36" viewBox="0 0 24 24" stroke-width="1.5" stroke="#fdd835" fill="none" stroke-linecap="round" stroke-linejoin="round">
                     <title id="discordTitleID">Discord</title>
