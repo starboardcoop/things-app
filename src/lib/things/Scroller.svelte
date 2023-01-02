97457cfde55c8dcce008e21ed465e4d5d652868a
Dillon Fagan <dillonfagan@pm.me>
2023-01-02 16:06:49 -0500

    Increase y gap in Thing grid

--- src/lib/things/Scroller.svelte
+++ src/lib/things/Scroller.svelte
@@ -2,7 +2,7 @@
     import Thing from "./Thing.svelte";
 
     export let wrap = false;
-    const style = wrap ? "grid grid-cols-3 md:grid-cols-5 gap-4 place-content-between" : "flex flex-row overflow-auto";
+    const style = wrap ? "grid grid-cols-3 md:grid-cols-5 gap-x-4 gap-y-6 place-content-between" : "flex flex-row overflow-auto";
 
     export let things = [];
     things = things.sort((a, b) => b.stock - a.stock);
