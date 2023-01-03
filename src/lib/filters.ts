3c8d11a46c10f7052ae6a31f813059049cc96091
Dillon Fagan <dillonfagan@pm.me>
2023-01-03 17:24:31 -0500

    Hide wish list items when toggle is off

--- src/lib/filters.ts
+++ src/lib/filters.ts
@@ -6,6 +6,8 @@ export const filter = (things, { keyword, onlyWishList, category }) => {
         filtered = filtered.filter(thing => thing.name.toLowerCase().includes(keyword.toLowerCase()));
     if (onlyWishList)
         filtered = filtered.filter(thing => thing.stock < 1);
+    if (!onlyWishList)
+        filtered = filtered.filter(thing => thing.stock !== 0);
 
     return filtered;
 }
