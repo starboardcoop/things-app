f86da0ee23666d49c1e23d0af94ae677e848536f
Dillon Fagan <dillonfagan@pm.me>
2023-01-12 08:01:05 -0500

    Add translations.ts

---
+++ src/lib/translations.ts
@@ -0,0 +1,10 @@
+export default {
+    english: {
+        "Button.Home": "Home",
+        "Button.Donate": "Donate",
+        "Button.WishList": "Wish List",
+        "Input.Search": "Search...",
+        "Thing.Tags.Available": "Available"
+    },
+    spanish: {}
+}
