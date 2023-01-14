4df8e864ee8361379efeb841e24a4ed2eaff7b89
Dillon Fagan <dillonfagan@pm.me>
2023-01-14 09:05:40 -0500

    Add Spanish translations

--- src/lib/language/translations.ts
+++ src/lib/language/translations.ts
@@ -3,9 +3,18 @@ export default {
         "Button.Home": "Home",
         "Button.Donate": "Donate",
         "Button.WishList": "Wish List",
+        "Chooser.CategoryPrompt": "Choose a category:",
         "Input.Search": "Search...",
         "Thing.Tags.Available": "Available",
         "A project by": "A project by"
     },
-    es: {}
+    es: {
+        "Button.Home": "Inicio",
+        "Button.Donate": "Donar",
+        "Button.WishList": "Donaciones",
+        "Chooser.CategoryPrompt": "Elija una categoría:",
+        "Input.Search": "Buscar...",
+        "Thing.Tags.Available": "Disponible",
+        "A project by": "Un proyecto de"
+    }
 }
