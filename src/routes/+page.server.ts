46cffc7ed1aae64f649270e5bd77564463c1ee75
Dillon Fagan <dillonfagan@pm.me>
2023-05-16 14:17:26 -0400

    Target local API without API_HOST env variable

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,6 @@
 export const load = async ({ fetch }) => {
-    const result = await fetch('https://starboardcoop-things-api.glitch.me/things');
+    const host = process.env.API_HOST ?? 'http://localhost:8088';
+    const result = await fetch(`${host}/things`);
     const data = await result.json();
     data.things = data.things.filter(thing => thing.categories);
 
